78973941f39c6e571b1d277206313d1535c7d958
Shuchita <shuchita.g1@gmail.com>
2018-10-30 22:50:30 +0530

    Made changes in the code. Removed unwanted lines of code.

--- Example.java
+++ Example.java
@@ -1,7 +1,6 @@
 public class Example{
 	public static void main(String []args){
-		Double d = Double.valueOf("1.34");
-		int a = Integer.parseInt("23");
+		
 		int s=0;	
 		for (int i=0;i<args.length;i++)
 			s+=Integer.parseInt(args[i]);
