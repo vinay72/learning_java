83252dc561cc6e148cfd37356d0c3c246b291652
Utkarsh Mittal <42753215+Stell1c@users.noreply.github.com>
2018-10-30 21:54:10 +0530

    new HelloWorld file added to repo
    used printf to print helloworld in java
    new hello world file added to repo.

---
+++ HelloWorlD.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class HelloWorlD
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		System.out.printf("Hello World!");
+	}
+}
