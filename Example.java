057af9dd7bd28cda0acc6ee6813f85d3a0a4f84f
Mukund Agarwal <m.agarwalhp@gmail.com>
2018-10-30 20:48:50 +0530

    removed comments

--- Example.java
+++ Example.java
@@ -1,10 +1,7 @@
 public class Example{
 	public static void main(String []args){
-	//	Integer i = Integer.valueOf("134");
 		Double d = Double.valueOf("1.34");
-	//	System.out.println(i+d);
 		int a = Integer.parseInt("23");
-	//	int x = i.intValue();
 		int s=0;	
 		for (int i=0;i<args.length;i++)
 			s+=Integer.parseInt(args[i]);
