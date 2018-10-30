da06692688dd95f24fdda8f5063928e9be32d940
itsmemukund <32777662+itsmemukund@users.noreply.github.com>
2018-10-30 20:39:49 +0530

    third file

---
+++ BeerBottles.java
@@ -0,0 +1,17 @@
+public class BeerBottles{
+	public static void main(String []args){
+		int n=99;
+		String word="bottles";
+
+		while(n>1){
+			System.out.println(n+" "+word+" of beer on the wall, "+n+" "+word+" of beer. ");
+			n--;
+			if(n==1)
+				word="bottle";	
+			System.out.println("Take one down and pass it around, "+n+" "+word+" of beer on the wall.");
+			System.out.println("");
+		}
+		System.out.println("1 bottle of beer on the wall, 1 bottle of beer.");
+		System.out.println("Go to the store and buy some more, 99 bottles of beer on the wall.");
+	}
+}
